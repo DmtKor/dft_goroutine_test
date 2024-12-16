6bb68db8b0a818524048e2c4643f29f348bd65e1
Dmitry Korablin <korablindm@gmail.com>
2024-12-16 17:03:46 +0300

    Updated shell script

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,7 +1,15 @@
-cd ./calc_src
-go build -o ../build/calc
-cd ../gen_data_src
-go build -o ../build/gen_data
-cd ..
-./build/gen_data 10000 > ./data/data.txt
-./build/calc < ./data/data.txt
+if [ $# -lt 1 ]; then
+    echo "Usage:" $0 "<data size>"
+else
+    if [ $1 -lt 100 ]; then 
+        echo "Data size should be greater or equal to 100"
+    else
+        cd ./calc_src
+        go build -o ../build/calc
+        cd ../gen_data_src
+        go build -o ../build/gen_data
+        cd ..
+        ./build/gen_data $1 > ./data/data.txt
+        ./build/calc < ./data/data.txt
+    fi
+fi
